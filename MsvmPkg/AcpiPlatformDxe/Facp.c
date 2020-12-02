e3c6ce6e63059506fe32301591acb4a40a7e5499
Mike Ebersol <mebersol@microsoft.com>
2020-12-02 23:50:19 +0000

    Merged PR 5386866: SNP VMs will be correctly HW-reduced
    SNP VMs will no longer be HW-reduced
    
    Related work items: #26402858, #30903954

--- MsvmPkg/AcpiPlatformDxe/Facp.c
+++ MsvmPkg/AcpiPlatformDxe/Facp.c
@@ -84,7 +84,6 @@ Return Value:
         facp->PreferredPmProfile = EFI_ACPI_6_2_PM_PROFILE_MOBILE;
     }
 
-
 #if defined(MDE_CPU_X64)
     // If this is a SNP VM, report it as hardware reduced. Zero out any of
     // filled in legacy structures.
@@ -95,6 +94,7 @@ Return Value:
              EFI_ACPI_6_2_PWR_BUTTON |
              EFI_ACPI_6_2_SLP_BUTTON |
              EFI_ACPI_6_2_TMR_VAL_EXT |
+             EFI_ACPI_6_2_RESET_REG_SUP |
              EFI_ACPI_6_2_HW_REDUCED_ACPI;
 
         // Zero out set fields between offsets 46 - 108
